3334480e42cd3c4b6bfe4f08d2f87802d32a7107
nithishkumar9521@gmail.com <nithishkumar9521@gmail.com>
2026-09-21 07:46:00 +0000

    Add database initialization script: create datawarehouse catalog with bronze, silver, and gold schemas

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,7 @@
+--created a catalog datawarehouse
+create catalog if not exists datawarehouse;
 
+--created schemas for datawarehouse as bronze, silver and gold
+create schema if not exists datawarehouse.bronze;
+create schema if not exists datawarehouse.silver;
+create schema if not exists datawarehouse.gold;
